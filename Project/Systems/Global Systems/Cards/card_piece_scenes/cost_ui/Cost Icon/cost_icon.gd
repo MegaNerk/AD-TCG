f89993e29102af35d17812aff645bc549d76dfc2
MegaNerk <odrecord@rcn.com>
2025-09-04 19:38:26 -0400

    Add: Text box to location

---
+++ Project/Systems/Global Systems/Cards/card_piece_scenes/cost_ui/Cost Icon/cost_icon.gd
@@ -0,0 +1,16 @@
+@tool
+extends Node
+
+@export var my_icon : Texture:
+	set(value):
+		my_icon = value
+		change_icon(my_icon)
+
+@onready var my_tex_rect = $MarginContainer/TextureRect
+
+func _ready():
+	change_icon(my_icon)
+
+func change_icon(new_icon : Texture):
+	if new_icon != null:
+		my_tex_rect.texture = new_icon
